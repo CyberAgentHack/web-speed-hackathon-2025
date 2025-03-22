57326f6c397261b45c09983f78affa73869b70aa
yuito.watanabe <w.ytmrm11811@gmail.com>
2025-03-22 14:12:13 +0900

    add: analyzer

--- workspaces/client/vite.config.ts
+++ workspaces/client/vite.config.ts
@@ -1,12 +1,26 @@
 import path from 'node:path';
 
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   assetsInclude: ['**/*.wasm'],
+  build: {
+    rollupOptions: {
+      plugins: [
+        mode === 'analyze' &&
+          visualizer({
+            brotliSize: true,
+            filename: 'dist/stats.html',
+            gzipSize: true,
+            open: true,
+          }),
+      ],
+    },
+  },
   define: {
     'process.env.API_BASE_URL': JSON.stringify('/api'),
     'process.env.NODE_ENV': JSON.stringify(process.env['NODE_ENV'] || 'development'),
@@ -31,4 +45,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
