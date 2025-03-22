5eb63749659448b8e88c00a1122cd69ad70df726
newt <ysti2019.win2020.st@gmail.com>
2025-03-22 17:49:32 +0900

    unocssの設定

--- workspaces/client/src/setups/unocss.ts
+++ workspaces/client/src/setups/unocss.ts
@@ -1,4 +1,5 @@
 import presetWind3 from '@unocss/preset-wind3';
+import resetCSS from '@unocss/reset/tailwind-compat.css?raw';
 import initUnocssRuntime, { defineConfig } from '@unocss/runtime';
 
 async function init() {
@@ -6,13 +7,12 @@ async function init() {
     defaults: defineConfig({
       layers: {
         default: 1,
-        icons: 0,
         preflights: 0,
         reset: -1,
       },
       preflights: [
         {
-          getCSS: () => import('@unocss/reset/tailwind-compat.css?raw').then(({ default: css }) => css),
+          getCSS: () => Promise.resolve(resetCSS),
           layer: 'reset',
         },
         {
