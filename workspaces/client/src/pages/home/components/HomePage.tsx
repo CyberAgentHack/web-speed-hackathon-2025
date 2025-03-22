9738a3fc8c5eb416bc4c8587fe56935460deb442
hkaomua <ko_hama@icloud.com>
2025-03-22 23:08:47 +0900

    Homepageの一部分のMemo化

--- workspaces/client/src/pages/home/components/HomePage.tsx
+++ workspaces/client/src/pages/home/components/HomePage.tsx
@@ -1,6 +1,7 @@
 import { createStore } from '@wsh-2025/client/src/app/createStore';
 import { RecommendedSection } from '@wsh-2025/client/src/features/recommended/components/RecommendedSection';
 import { useRecommended } from '@wsh-2025/client/src/features/recommended/hooks/useRecommended';
+import { useMemo } from 'react';
 
 export const prefetch = async (store: ReturnType<typeof createStore>) => {
   const modules = await store
@@ -12,18 +13,20 @@ export const prefetch = async (store: ReturnType<typeof createStore>) => {
 export const HomePage = () => {
   const modules = useRecommended({ referenceId: 'entrance' });
 
+  // modules が変更されない限り再計算しない（ここでは不要な場合もあるが、処理が重いなら有効）
+  const renderedModules = useMemo(() => {
+    return modules.map(module => (
+      <div key={module.id} className="mb-[24px] px-[24px]">
+        <RecommendedSection module={module} />
+      </div>
+    ));
+  }, [modules]);  
+
   return (
     <>
       <title>Home - AremaTV</title>
-
       <div className="w-full py-[48px]">
-        {modules.map((module) => {
-          return (
-            <div key={module.id} className="mb-[24px] px-[24px]">
-              <RecommendedSection module={module} />
-            </div>
-          );
-        })}
+        {renderedModules}
       </div>
     </>
   );
