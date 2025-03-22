e73ecc7bfa8154d5d022b1c003e6d8672c03adb0
mutsumiyamaguchi <tayomuwa2020@gmail.com>
2025-03-22 14:17:54 +0900

    fixed webapp

--- workspaces/client/src/pages/home/components/HomePage.tsx
+++ workspaces/client/src/pages/home/components/HomePage.tsx
@@ -1,48 +1,15 @@
-// import { createStore } from '@wsh-2025/client/src/app/createStore';
-// import { RecommendedSection } from '@wsh-2025/client/src/features/recommended/components/RecommendedSection';
-// import { useRecommended } from '@wsh-2025/client/src/features/recommended/hooks/useRecommended';
-
-// export const prefetch = async (store: ReturnType<typeof createStore>) => {
-//   const modules = await store
-//     .getState()
-//     .features.recommended.fetchRecommendedModulesByReferenceId({ referenceId: 'entrance' });
-//   return { modules };
-// };
-
-// export const HomePage = () => {
-//   const modules = useRecommended({ referenceId: 'entrance' });
-
-//   return (
-//     <>
-//       <title>Home - AremaTV</title>
-
-//       <div className="w-full py-[48px]">
-//         {modules.map((module) => {
-//           return (
-//             <div key={module.id} className="mb-[24px] px-[24px]">
-//               <RecommendedSection module={module} />
-//             </div>
-//           );
-//         })}
-//       </div>
-//     </>
-//   );
-// };
-
 import { createStore } from '@wsh-2025/client/src/app/createStore';
 import { RecommendedSection } from '@wsh-2025/client/src/features/recommended/components/RecommendedSection';
 import { useRecommended } from '@wsh-2025/client/src/features/recommended/hooks/useRecommended';
 
 export const prefetch = async (store: ReturnType<typeof createStore>) => {
-  // サーバーサイドでデータを事前に取得
   const modules = await store
     .getState()
     .features.recommended.fetchRecommendedModulesByReferenceId({ referenceId: 'entrance' });
-  return { modules }; // 事前に取得したモジュールを返す
+  return { modules };
 };
 
 export const HomePage = () => {
-  // クライアントサイドでのデータ取得
   const modules = useRecommended({ referenceId: 'entrance' });
 
   return (
@@ -50,17 +17,13 @@ export const HomePage = () => {
       <title>Home - AremaTV</title>
 
       <div className="w-full py-[48px]">
-        {/* モジュールがまだない場合はローディング状態を表示 */}
-        {!modules.length ? (
-          <div>Loading...</div>
-        ) : (
-          modules.map((module) => (
+        {modules.map((module) => {
+          return (
             <div key={module.id} className="mb-[24px] px-[24px]">
-              {/* React.memo を使って RecommendedSection コンポーネントをメモ化 */}
               <RecommendedSection module={module} />
             </div>
-          ))
-        )}
+          );
+        })}
       </div>
     </>
   );
