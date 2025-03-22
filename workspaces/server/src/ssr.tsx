a185fae164c659f845829e0d52f9e724b8aad819
newt <ysti2019.win2020.st@gmail.com>
2025-03-22 13:26:18 +0900

    __staticRouterHydrationDataを削除してみる

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -7,7 +7,6 @@ import { createRoutes } from '@wsh-2025/client/src/app/createRoutes';
 import { createStore } from '@wsh-2025/client/src/app/createStore';
 import type { FastifyInstance } from 'fastify';
 import { createStandardRequest } from 'fastify-standard-request-reply';
-import htmlescape from 'htmlescape';
 import { StrictMode } from 'react';
 import { renderToString } from 'react-dom/server';
 import { createStaticHandler, createStaticRouter, StaticRouterProvider } from 'react-router';
@@ -56,12 +55,6 @@ export function registerSsr(app: FastifyInstance): void {
         </head>
         <body></body>
       </html>
-      <script>
-        window.__staticRouterHydrationData = ${htmlescape({
-          actionData: context.actionData,
-          loaderData: context.loaderData,
-        })};
-      </script>
     `);
   });
 }
