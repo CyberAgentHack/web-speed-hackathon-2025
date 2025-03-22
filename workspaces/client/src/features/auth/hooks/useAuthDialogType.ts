87593c8e4d6ca4ecf731ffb1ab2e49e7948ccdc3
WatermelonBomb <coins07311102@gmail.com>
2025-03-23 00:18:30 +0900

    Update useAuthDialogType.ts

--- workspaces/client/src/features/auth/hooks/useAuthDialogType.ts
+++ workspaces/client/src/features/auth/hooks/useAuthDialogType.ts
@@ -1,6 +1,6 @@
 import { useStore } from '@wsh-2025/client/src/app/StoreContext';
 
 export function useAuthDialogType() {
-  const state = useStore((s) => s);
-  return state.features.auth.dialog;
+  // ダイアログ種別だけをセレクト
+  return useStore((s) => s.features.auth.dialog);
 }
