c9a84d6526d358e9ac07611890d6fbdcc748f5d8
fujiyamaorange <kaito.bakuman@gmail.com>
2025-03-22 14:34:15 +0900

    refactor: remove bi

--- workspaces/client/src/features/layout/components/Layout.tsx
+++ workspaces/client/src/features/layout/components/Layout.tsx
@@ -87,7 +87,14 @@ export const Layout = ({ children }: Props) => {
               className="block flex h-[56px] w-[188px] items-center justify-center pb-[8px] pl-[20px] pr-[8px] pt-[8px]"
               to="/"
             >
-              <div className="i-bi:house-fill m-[4px] size-[20px] shrink-0 grow-0" />
+              {/* <div className="i-bi:house-fill m-[4px] size-[20px] shrink-0 grow-0" /> */}
+              <svg className="m-[4px] size-[20px] shrink-0 grow-0" viewBox="0 0 16 16">
+                <g fill="currentColor">
+                  <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z" />
+                  <path d="m8 3.293l6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z" />
+                </g>
+              </svg>
+
               <span className="grow-1 shrink-1 ml-[16px] text-left text-[14px] font-bold">ホーム</span>
             </Link>
 
