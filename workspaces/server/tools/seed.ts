1ca976d892cfc6ef7bda0764afc261e9e9bfc4d7
maru919 <kento64maruoka@gmail.com>
2025-03-22 19:41:03 +0900

    feat: Fix layouts

--- workspaces/server/tools/seed.ts
+++ workspaces/server/tools/seed.ts
@@ -91,7 +91,7 @@ async function main() {
   const files = await getFiles(path.resolve(rootDir, 'public/images'));
   const imagePaths = files.map((file) => path.join('/', path.relative(rootDir, file)));
 
-  console.log("imagePaths in seed.ts", imagePaths)
+  console.log('imagePaths in seed.ts', imagePaths);
 
   try {
     const animeList = await fetchAnimeList();
