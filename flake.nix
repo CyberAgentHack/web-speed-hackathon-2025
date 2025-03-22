9b3fa794ea971c1f674f4b8a314652354c25adcf
asa1984 <satoasa9913@gmail.com>
2025-03-22 17:12:20 +0900

    init: flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-linux" # 64-bit x86 Linux
+        "aarch64-darwin" # 64-bit ARM macOS
+        "x86_64-darwin" # 64-bit x86 macOS
+      ];
+
+      perSystem =
+        {
+          pkgs,
+          ...
+        }:
+        {
+          devShells = {
+            default = pkgs.mkShell {
+              buildInputs = with pkgs; [
+                nodejs-slim_22
+                corepack
+              ];
+            };
+          };
+        };
+    };
+}
