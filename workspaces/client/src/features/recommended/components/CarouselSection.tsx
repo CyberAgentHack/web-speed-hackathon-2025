b5ce312b9790af78328324e9ff880f441565259c
taisei-13046 <yasui-edu0834@toki.waseda.jp>
2025-03-22 15:12:48 +0900

    ScrollRestorationの削除

--- workspaces/client/src/features/recommended/components/CarouselSection.tsx
+++ workspaces/client/src/features/recommended/components/CarouselSection.tsx
@@ -1,4 +1,3 @@
-import { ElementScrollRestoration } from '@epic-web/restore-scroll';
 import { StandardSchemaV1 } from '@standard-schema/spec';
 import * as schema from '@wsh-2025/schema/src/api/schema';
 import { ArrayValues } from 'type-fest';
@@ -26,7 +25,6 @@ export const CarouselSection = ({ module }: Props) => {
           key={module.id}
           ref={mergedRef}
           className={`relative mx-[-24px] flex flex-row gap-x-[12px] overflow-x-auto overflow-y-hidden pl-[24px] pr-[56px]`}
-          data-scroll-restore={`carousel-${module.id}`}
         >
           {module.items.map((item) => (
             <div key={item.id} className={`w-[${itemWidth}px] shrink-0 grow-0`}>
@@ -36,8 +34,6 @@ export const CarouselSection = ({ module }: Props) => {
           ))}
         </div>
       </div>
-
-      <ElementScrollRestoration direction="horizontal" elementQuery={`[data-scroll-restore="carousel-${module.id}"]`} />
     </>
   );
 };
