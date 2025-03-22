56f497810238f4ad95691249892506aab2adb41a
ayumu-1212 <ayumuabe1434@gmail.com>
2025-03-22 16:11:15 +0900

    remove: fluent

--- workspaces/client/src/setups/unocss.ts
+++ workspaces/client/src/setups/unocss.ts
@@ -51,7 +51,6 @@ async function init() {
           collections: {
             'fa-solid': () =>
               import('@iconify/json/json/fa-solid.json').then((m): IconifyJSON => m.default as IconifyJSON),
-            fluent: () => import('@iconify/json/json/fluent.json').then((m): IconifyJSON => m.default as IconifyJSON),
             'line-md': () =>
               import('@iconify/json/json/line-md.json').then((m): IconifyJSON => m.default as IconifyJSON),
             'material-symbols': () =>
