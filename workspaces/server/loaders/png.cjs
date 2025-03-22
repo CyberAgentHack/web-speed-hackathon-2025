a284d20ab542271ba5dc0bd78dd3b5c9551283dd
TaikiKuwahara <un2h0yc31@gmail.com>
2025-03-22 11:19:32 +0900

    fix: loaders/png.cjs 処理を非同期化・キャッシュを有効化

--- workspaces/server/loaders/png.cjs
+++ workspaces/server/loaders/png.cjs
@@ -1,9 +1,22 @@
 /* eslint-disable */
 
 const Module = require('module');
-const fs = require('fs');
+const fs = require('fs').promises;
+cache = new Map()
 
-Module._extensions['.png'] = function (module, fn) {
-  const base64 = fs.readFileSync(fn).toString('base64');
-  module._compile('module.exports="data:image/png;base64,' + base64 + '"', fn);
+Module._extensions['.png'] = async function (module, fn) {
+  if (cache.has(fn)) {
+    module._compile(`module.exports=${JSON.stringify(cache.get(fn))}`, fn);
+    return;
+  }
+
+  try {
+    const data = await fs.readFile(fn); // 非同期化
+    const base64 = `data:image/png;base64,${data.toString('base64')}`;
+    
+    cache.set(fn, base64); // キャッシュを使用
+    module._compile(`module.exports=${JSON.stringify(base64)}`, fn);
+  } catch (error) {
+    throw new Error(`Error loading PNG file: ${fn} - ${error.message}`);
+  }
 };
