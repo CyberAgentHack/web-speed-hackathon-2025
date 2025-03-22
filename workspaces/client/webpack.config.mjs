82eca25dc505232aec5446278ed41df6c1637bb7
AkitoYamashita <akt.ymst@gmail.com>
2025-03-23 03:59:50 +0900

    fix
    (cherry picked from commit a23b5be080f7bbe1dc38550a70c73af854e3be56)

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -1,12 +1,13 @@
 import path from 'node:path';
 
+import {BundleAnalyzerPlugin} from 'webpack-bundle-analyzer';
 import webpack from 'webpack';
 
 /** @type {import('webpack').Configuration} */
 const config = {
-  devtool: 'inline-source-map',
+  devtool: false,
   entry: './src/main.tsx',
-  mode: 'none',
+  mode: 'production',
   module: {
     rules: [
       {
@@ -31,6 +32,8 @@ const config = {
               ['@babel/preset-react', { runtime: 'automatic' }],
               ['@babel/preset-typescript'],
             ],
+            cacheDirectory: true,
+            cacheCompression: true,
           },
         },
       },
@@ -57,10 +60,16 @@ const config = {
     filename: 'main.js',
     path: path.resolve(import.meta.dirname, './dist'),
     publicPath: 'auto',
+    clean: true,
   },
   plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
-    new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: '' }),
+    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 10 }),
+    new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: 'production' }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static",
+      reportFilename: "../analyze.html",
+      openAnalyzer: false
+    }),
   ],
   resolve: {
     alias: {
