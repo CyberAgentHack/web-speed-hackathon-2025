a924691665e87fa74b1fe311dfe958f67b1bbabf
nakamuraitsuki <ituki1911n@gmail.com>
2025-03-22 19:38:05 +0900

    fix: スクロールを修正（復元）

--- workspaces/client/src/features/recommended/hooks/useScrollSnap.ts
+++ workspaces/client/src/features/recommended/hooks/useScrollSnap.ts
@@ -3,7 +3,7 @@ import { useEffect, useRef } from 'react';
 export function useScrollSnap({ scrollPadding }: { scrollPadding: number }) {
   const containerRef = useRef<HTMLDivElement>(null);
   const isScrolling = useRef(false);
-  const isSnapping = useRef<ReturnType<typeof setTimeout> | undefined>(undefined);
+  const isSnapping = useRef(false);
 
   useEffect(() => {
     if (containerRef.current == null) {
@@ -15,40 +15,61 @@ export function useScrollSnap({ scrollPadding }: { scrollPadding: number }) {
         return;
       }
       isScrolling.current = true;
-
-      // Scroll処理が終了した後にsnapを実行
-      clearTimeout(isSnapping.current);
-      isSnapping.current = setTimeout(() => {
-        if (!containerRef.current) return;
-
-        const childElements = Array.from(containerRef.current.children) as HTMLElement[];
-        const childScrollPositions = childElements.map((element) => element.offsetLeft);
-        const scrollPosition = containerRef.current.scrollLeft;
-        const childIndex = childScrollPositions.reduce((prev, curr, index) => {
-          return Math.abs(curr - scrollPosition) < Math.abs((childScrollPositions[prev] ?? 0) - scrollPosition)
-            ? index
-            : prev;
-        }, 0);
-
-        // Snap処理を実行
-        containerRef.current.scrollTo({
-          behavior: 'smooth',
-          left: (childScrollPositions[childIndex] ?? 0) - scrollPadding,
-        });
-
-        isScrolling.current = false;
-      }, 150); // 適切な遅延を設定
     };
 
+    const handleScrollend = () => {
+      if (!isScrolling.current) {
+        return;
+      }
+      isScrolling.current = false;
+    };
+
+    let timer: ReturnType<typeof setTimeout> | null = null;
+    let interval = setInterval(() => {
+      if (!containerRef.current) {
+        return;
+      }
+
+      const childElements = Array.from(containerRef.current.children) as HTMLElement[];
+      const childScrollPositions = childElements.map((element) => element.offsetLeft);
+      const scrollPosition = containerRef.current.scrollLeft;
+      const childIndex = childScrollPositions.reduce((prev, curr, index) => {
+        return Math.abs(curr - scrollPosition) < Math.abs((childScrollPositions[prev] ?? 0) - scrollPosition)
+          ? index
+          : prev;
+      }, 0);
+
+      if (isScrolling.current) {
+        return;
+      }
+
+      if (isSnapping.current) {
+        return;
+      }
+
+      isSnapping.current = true;
+      containerRef.current.scrollTo({
+        behavior: 'smooth',
+        left: (childScrollPositions[childIndex] ?? 0) - scrollPadding,
+      });
+
+      timer = setTimeout(() => {
+        isSnapping.current = false;
+      }, 1000);
+    });
+
     containerRef.current.addEventListener('scroll', handleScroll);
+    containerRef.current.addEventListener('scrollend', handleScrollend);
 
     return () => {
       containerRef.current?.removeEventListener('scroll', handleScroll);
-      if (isSnapping.current) {
-        clearTimeout(isSnapping.current);
+      containerRef.current?.removeEventListener('scrollend', handleScrollend);
+      clearInterval(interval);
+      if (timer) {
+        clearTimeout(timer);
       }
     };
-  }, [scrollPadding]);
+  }, []);
 
   return containerRef;
 }
