c08e14d3b35945a90328eb8eaec8e4bc32a3cd2a
Toshiki Itai <toshiki.itai@linecorp.com>
2025-03-22 20:19:29 +0900

    chore: ssrを一旦やめる

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -2,14 +2,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import fastifyStatic from '@fastify/static';
-import { StoreProvider } from '@wsh-2025/client/src/app/StoreContext';
-import { createRoutes } from '@wsh-2025/client/src/app/createRoutes';
-import { createStore } from '@wsh-2025/client/src/app/createStore';
 import type { FastifyInstance } from 'fastify';
-import { createStandardRequest } from 'fastify-standard-request-reply';
-import { StrictMode } from 'react';
-import { renderToString } from 'react-dom/server';
-import { createStaticHandler, createStaticRouter, StaticRouterProvider } from 'react-router';
 
 export function registerSsr(app: FastifyInstance): void {
   app.register(fastifyStatic, {
@@ -24,30 +17,19 @@ export function registerSsr(app: FastifyInstance): void {
     reply.status(404).send();
   });
 
-  app.get('/*', async (req, reply) => {
-    // @ts-expect-error ................
-    const request = createStandardRequest(req, reply);
-
-    const store = createStore({});
-    const handler = createStaticHandler(createRoutes(store));
-    const context = await handler.query(request);
-
-    if (context instanceof Response) {
-      return reply.send(context);
-    }
-
-    const router = createStaticRouter(handler.dataRoutes, context);
-    const html = renderToString(
-      <StrictMode>
-        <StoreProvider createStore={() => store}>
-          <StaticRouterProvider context={context} router={router} />
-        </StoreProvider>
-      </StrictMode>,
-    );
-
+  app.get('/*', async (_, reply) => {
     reply.type('text/html').send(/* html */ `
       <!DOCTYPE html>
-      ${html}
+      <html className="size-full" lang="ja">
+        <head>
+          <meta charSet="UTF-8" />
+          <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+          <script src="/public/main.js"></script>
+        </head>
+        <body className="size-full bg-[#000000] text-[#ffffff]">
+          <div id="root"></div>
+        </body>
+      </html>
     `);
   });
 }
