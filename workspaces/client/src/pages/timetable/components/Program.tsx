07da49060df4eae9a1d8859e27c61fc3b10b11b9
newt <ysti2019.win2020.st@gmail.com>
2025-03-23 05:04:02 +0900

    wip

--- workspaces/client/src/pages/timetable/components/Program.tsx
+++ workspaces/client/src/pages/timetable/components/Program.tsx
@@ -69,9 +69,10 @@ export const Program = ({ height, program }: Props): ReactElement => {
             <img
               ref={imageRef}
               alt=""
-              className="pointer-events-none w-full rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F]"
+              className="pointer-events-none aspect-video w-full rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F]"
               loading="lazy"
               src={program.thumbnailUrl}
+              width={254}
             />
           </div>
         </div>
