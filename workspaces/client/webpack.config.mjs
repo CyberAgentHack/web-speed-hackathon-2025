c8f913c18dd225961b7e0f53b77e5e73dddf175b
hi mochizuki <hi.mochizuki@buysell-technologies.com>
2025-03-22 17:02:19 +0900

    node_env を production に

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -69,7 +69,7 @@ const config = {
   plugins: [
     // new BundleAnalyzerPlugin(),
     // new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
-    new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: '' }),
+    new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: 'production' }),
     // new BundleAnalyzerPlugin(),
   ],
   resolve: {
