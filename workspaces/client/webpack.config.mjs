1461e48491c74a3c28bf0d06452571a5568cba48
tosaken1116 <tosaken.dev@gmail.com>
2025-03-22 17:54:06 +0900

    Set Webpack mode to production and enable optimization for better performance

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -6,7 +6,7 @@ import webpack from 'webpack';
 const config = {
   devtool: 'inline-source-map',
   entry: './src/main.tsx',
-  mode: 'none',
+  mode: 'production',
   module: {
     rules: [
       {
@@ -51,6 +51,9 @@ const config = {
       },
     ],
   },
+  optimization: {
+    minimize: true,
+  },
   output: {
     chunkFilename: 'chunk-[contenthash].js',
     chunkFormat: false,
@@ -60,7 +63,7 @@ const config = {
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
-    new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: '' }),
+    new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: 'production' }),
   ],
   resolve: {
     alias: {
