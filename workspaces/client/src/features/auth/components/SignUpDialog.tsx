fb9a56a059328c3c28a35768a06339674427fd68
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2025-03-22 23:36:49 +0900

    icon

--- workspaces/client/src/features/auth/components/SignUpDialog.tsx
+++ workspaces/client/src/features/auth/components/SignUpDialog.tsx
@@ -121,7 +121,14 @@ export const SignUpDialog = ({ isOpen, onClose, onOpenSignIn }: Props) => {
 
               {submitError ? (
                 <div className="mb-[8px] flex w-full flex-row items-center justify-start rounded-[4px] border-[2px] border-solid border-[#F0163A] bg-[#ffeeee] p-[8px] text-[14px] font-bold text-[#F0163A]">
-                  <div className="i-material-symbols:error-outline m-[4px] size-[20px]" />
+                  <div className="m-[4px] size-[20px]">
+                    <svg viewBox="0 0 24 24">
+                      <path
+                        fill="currentColor"
+                        d="M2.725 21q-.275 0-.5-.137t-.35-.363t-.137-.488t.137-.512l9.25-16q.15-.25.388-.375T12 3t.488.125t.387.375l9.25 16q.15.25.138.513t-.138.487t-.35.363t-.5.137zm1.725-2h15.1L12 6zM12 18q.425 0 .713-.288T13 17t-.288-.712T12 16t-.712.288T11 17t.288.713T12 18m0-3q.425 0 .713-.288T13 14v-3q0-.425-.288-.712T12 10t-.712.288T11 11v3q0 .425.288.713T12 15m0-2.5"
+                      />
+                    </svg>
+                  </div>
                   <span>{submitError}</span>
                 </div>
               ) : null}
