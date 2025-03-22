f046ec87fcf7f9112358c5d14670082cf2f16db0
K-Kizuku <kotani.kizuku@gmail.com>
2025-03-22 13:48:28 +0900

    revert

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -4,33 +4,30 @@ import webpack from 'webpack';
 
 /** @type {import('webpack').Configuration} */
 const config = {
-  mode: 'production',
-  devtool: 'source-map',
-
-  cache: { type: 'filesystem' },
-
+  devtool: 'inline-source-map',
   entry: './src/main.tsx',
-
-  output: {
-    path: path.resolve(import.meta.dirname, './dist'),
-    filename: 'main.js',
-    chunkFilename: 'chunk-[contenthash].js',
-    publicPath: 'auto',
-    chunkFormat: false,
-  },
+  mode: 'none',
   module: {
     rules: [
       {
-        test: /\.(?:js|mjs|cjs|jsx|ts|mts|cts|tsx)$/,
         exclude: [/node_modules\/video\.js/, /node_modules\/@videojs/],
-        resolve: { fullySpecified: false },
+        resolve: {
+          fullySpecified: false,
+        },
+        test: /\.(?:js|mjs|cjs|jsx|ts|mts|cts|tsx)$/,
         use: {
           loader: 'babel-loader',
           options: {
-            cacheDirectory: true,
-            cacheCompression: false,
             presets: [
-              ['@babel/preset-env', { forceAllTransforms: true, targets: { chrome: '134' }, useBuiltIns: false }],
+              [
+                '@babel/preset-env',
+                {
+                  corejs: '3.41',
+                  forceAllTransforms: true,
+                  targets: 'defaults',
+                  useBuiltIns: 'entry',
+                },
+              ],
               ['@babel/preset-react', { runtime: 'automatic' }],
               ['@babel/preset-typescript'],
             ],
@@ -39,27 +36,32 @@ const config = {
       },
       {
         test: /\.png$/,
-        type: 'asset/resource',
-        parser: { dataUrlCondition: { maxSize: 1024 } },
+        type: 'asset/inline',
       },
-      { resourceQuery: /raw/, type: 'asset/source' },
-      { resourceQuery: /arraybuffer/, type: 'javascript/auto', use: 'arraybuffer-loader' },
+      {
+        resourceQuery: /raw/,
+        type: 'asset/source',
+      },
+      {
+        resourceQuery: /arraybuffer/,
+        type: 'javascript/auto',
+        use: {
+          loader: 'arraybuffer-loader',
+        },
+      },
     ],
   },
-
-  optimization: {
-    minimize: true,
-    moduleIds: 'deterministic',
-    // runtimeChunk: 'single',
-    splitChunks: false,
+  output: {
+    chunkFilename: 'chunk-[contenthash].js',
+    chunkFormat: false,
+    filename: 'main.js',
+    path: path.resolve(import.meta.dirname, './dist'),
+    publicPath: 'auto',
   },
-
   plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
     new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: '' }),
-    new webpack.ids.HashedModuleIdsPlugin(),
-    // new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
-  ].filter(Boolean),
-
+  ],
   resolve: {
     alias: {
       '@ffmpeg/core$': path.resolve(import.meta.dirname, 'node_modules', '@ffmpeg/core/dist/umd/ffmpeg-core.js'),
