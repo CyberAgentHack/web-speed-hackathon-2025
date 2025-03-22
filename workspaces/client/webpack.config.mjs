8b380cd34f98ef10a689882d89816108e777f3f0
Tatsuki Sugiura <sugi@nemui.org>
2025-03-22 17:50:34 +0900

    Tune build.

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -1,12 +1,28 @@
 import path from 'node:path';
+import { env } from 'node:process';
 
 import webpack from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+
+const mode = env['NODE_ENV'] === "production" ? 'production' : 'development';
+
+/** @type {import('webpack').Configuration['plugins']} */
+const plugins =
+  [
+    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
+    new webpack.EnvironmentPlugin({ API_BASE_URL: '/api' }),
+  ]
+
+if (mode === 'development') {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call
+  plugins.push(new BundleAnalyzerPlugin());
+}
 
 /** @type {import('webpack').Configuration} */
 const config = {
-  devtool: 'inline-source-map',
+  devtool: 'source-map',
   entry: './src/main.tsx',
-  mode: 'none',
+  mode,
   module: {
     rules: [
       {
@@ -35,10 +51,6 @@ const config = {
         },
       },
       {
-        test: /\.png$/,
-        type: 'asset/inline',
-      },
-      {
         resourceQuery: /raw/,
         type: 'asset/source',
       },
@@ -58,10 +70,7 @@ const config = {
     path: path.resolve(import.meta.dirname, './dist'),
     publicPath: 'auto',
   },
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
-    new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: '' }),
-  ],
+  plugins,
   resolve: {
     alias: {
       '@ffmpeg/core$': path.resolve(import.meta.dirname, 'node_modules', '@ffmpeg/core/dist/umd/ffmpeg-core.js'),
