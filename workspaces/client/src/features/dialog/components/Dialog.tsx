fbc58162bde692f6e287244b038c919479a2b5d6
kq5y <tkser.edu@gmail.com>
2025-03-22 17:40:41 +0900

    refactor: Headless UIのDialogコンポーネントのインポートを最適化

--- workspaces/client/src/features/dialog/components/Dialog.tsx
+++ workspaces/client/src/features/dialog/components/Dialog.tsx
@@ -1,4 +1,4 @@
-import * as HeadlessUi from '@headlessui/react';
+import { Dialog as _Dialog, DialogPanel } from '@headlessui/react';
 import { ReactNode } from 'react';
 
 interface Props {
@@ -9,14 +9,14 @@ interface Props {
 
 export const Dialog = ({ children, isOpen, onClose }: Props) => {
   return (
-    <HeadlessUi.Dialog
+    <_Dialog
       className="fixed inset-0 z-50 flex items-center justify-center bg-[#00000077]"
       open={isOpen}
       onClose={onClose}
     >
-      <HeadlessUi.DialogPanel className="w-[480px] shrink-0 grow-0 rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F] bg-[#171717] px-[16px] py-[32px]">
+      <DialogPanel className="w-[480px] shrink-0 grow-0 rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F] bg-[#171717] px-[16px] py-[32px]">
         {children}
-      </HeadlessUi.DialogPanel>
-    </HeadlessUi.Dialog>
+      </DialogPanel>
+    </_Dialog>
   );
 };
