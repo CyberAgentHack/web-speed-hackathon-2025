5f9dccb19782e97d6c5227b30e1aebfb4e8a8fd3
kq5y <tkser.edu@gmail.com>
2025-03-22 23:09:28 +0900

    hoverableを消す

--- workspaces/client/src/pages/program/components/PlayerController.tsx
+++ workspaces/client/src/pages/program/components/PlayerController.tsx
@@ -1,5 +1,4 @@
 import { Icon } from '@iconify/react';
-import { Hoverable } from '@wsh-2025/client/src/features/layout/components/Hoverable';
 import { useMuted } from '@wsh-2025/client/src/pages/program/hooks/useMuted';
 
 export const PlayerController = () => {
@@ -17,18 +16,16 @@ export const PlayerController = () => {
           </div>
 
           <div className="flex flex-row items-center">
-            <Hoverable classNames={{ default: 'bg-transparent', hovered: 'bg-[#FFFFFF1F]' }}>
-              <button
-                aria-label={muted ? 'ミュート解除する' : 'ミュートする'}
-                className="block rounded-[4px]"
-                type="button"
-                onClick={() => {
-                  toggleMuted();
-                }}
-              >
-                <Icon icon={`material-symbols:${muted ? 'volume-off-rounded' : 'volume-up-rounded'}`} className="m-[14px] block size-[20px] shrink-0 grow-0 text-[#FFFFFF]" />
-              </button>
-            </Hoverable>
+            <button
+              aria-label={muted ? 'ミュート解除する' : 'ミュートする'}
+              className="block rounded-[4px] bg-transparent hover:bg-[#FFFFFF1F]"
+              type="button"
+              onClick={() => {
+                toggleMuted();
+              }}
+            >
+              <Icon icon={`material-symbols:${muted ? 'volume-off-rounded' : 'volume-up-rounded'}`} className="m-[14px] block size-[20px] shrink-0 grow-0 text-[#FFFFFF]" />
+            </button>
           </div>
         </div>
       </div>
