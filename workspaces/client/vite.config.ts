918f27f9ae7b71dbf808218b7bf021c2f3de4306
asa1984 <satoasa9913@gmail.com>
2025-03-23 01:21:12 +0900

    chore: add analyzer

--- workspaces/client/vite.config.ts
+++ workspaces/client/vite.config.ts
@@ -1,8 +1,22 @@
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  build: {
+    rollupOptions: {
+      plugins: [
+        mode === 'analyze' &&
+          visualizer({
+            filename: 'stats.html',
+            gzipSize: true,
+            open: true,
+          }),
+      ],
+    },
+  },
+
   define: {
     'process.env.API_BASE_URL': JSON.stringify('/api'),
     'process.env.NODE_ENV': JSON.stringify(''),
@@ -15,4 +29,4 @@ export default defineConfig({
   plugins: [tsconfigPaths(), react()],
 
   publicDir: '../../public',
-});
+}));
