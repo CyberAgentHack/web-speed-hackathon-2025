be3248ccff41a41a3d1aa639100413a855a63b90
pishiko <55dorakoro@gmail.com>
2025-03-23 04:02:04 +0900

    ...

--- workspaces/client/src/features/series/hooks/useSeriesById.ts
+++ workspaces/client/src/features/series/hooks/useSeriesById.ts
@@ -1,10 +1,19 @@
+import { StandardSchemaV1 } from '@standard-schema/spec';
 import { useStore } from '@wsh-2025/client/src/app/StoreContext';
+import * as schema from '@wsh-2025/schema/src/api/schema';
+import { useLoaderData } from 'react-router';
 
 interface Params {
   seriesId: string;
 }
 
 export function useSeriesById({ seriesId }: Params) {
+  const { series: prefetchedSeries } = useLoaderData() as {
+    series: StandardSchemaV1.InferOutput<typeof schema.getSeriesByIdResponse>;
+  };
+  if (prefetchedSeries) {
+    return prefetchedSeries;
+  }
   const state = useStore((s) => s);
 
   const series = state.features.series.series[seriesId];
