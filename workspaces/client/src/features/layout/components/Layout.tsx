4def32708472583481cd2886f0e7012ad6021b7f
Akatsuki1910 <35941346+Akatsuki1910@users.noreply.github.com>
2025-03-23 00:39:38 +0900

    Update Layout.tsx

--- workspaces/client/src/features/layout/components/Layout.tsx
+++ workspaces/client/src/features/layout/components/Layout.tsx
@@ -75,7 +75,7 @@ export const Layout = ({ children }: Props) => {
               type="button"
               onClick={isSignedIn ? authActions.openSignOutDialog : authActions.openSignInDialog}
             >
-              <div className={`m-[4px] size-[20px] shrink-0 grow-0`}>
+              <div className={`d-flex align-items-center m-[4px] size-[20px] shrink-0 grow-0`}>
                 {isSignedIn ? (
                   <svg viewBox="0 0 32 32">
                     <path
@@ -84,7 +84,7 @@ export const Layout = ({ children }: Props) => {
                     />
                   </svg>
                 ) : (
-                  <svg viewBox="0 0 32 448">
+                  <svg viewBox="0 0 448 512">
                     <path
                       fill="currentColor"
                       d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0S96 57.3 96 128s57.3 128 128 128m89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4"
@@ -116,8 +116,8 @@ export const Layout = ({ children }: Props) => {
               className="block flex h-[56px] w-[188px] items-center justify-center pb-[8px] pl-[20px] pr-[8px] pt-[8px]"
               to="/timetable"
             >
-              <div className="m-[4px] size-[20px] shrink-0 grow-0">
-                <svg viewBox="0 0 448 32">
+              <div className="d-flex align-items-center m-[4px] size-[20px] shrink-0 grow-0">
+                <svg viewBox="0 0 448 512">
                   <path
                     fill="currentColor"
                     d="M12 192h424c6.6 0 12 5.4 12 12v260c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V204c0-6.6 5.4-12 12-12m436-44v-36c0-26.5-21.5-48-48-48h-48V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H160V12c0-6.6-5.4-12-12-12h-40c-6.6 0-12 5.4-12 12v52H48C21.5 64 0 85.5 0 112v36c0 6.6 5.4 12 12 12h424c6.6 0 12-5.4 12-12"
