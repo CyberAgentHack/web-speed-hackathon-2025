8f4d61b291bd32b6499b498be1ec175bab775519
newt <ysti2019.win2020.st@gmail.com>
2025-03-23 07:43:11 +0900

    react-flip-toolkitを削除 いまいち用途がわからん

--- workspaces/client/src/features/recommended/components/SeriesItem.tsx
+++ workspaces/client/src/features/recommended/components/SeriesItem.tsx
@@ -1,4 +1,3 @@
-import { Flipped } from 'react-flip-toolkit';
 import { NavLink } from 'react-router';
 
 interface Props {
@@ -13,25 +12,12 @@ interface Props {
 export const SeriesItem = ({ first = false, series }: Props) => {
   return (
     <NavLink viewTransition className="block w-full overflow-hidden hover:opacity-75" to={`/series/${series.id}`}>
-      {({ isTransitioning }) => {
-        return (
-          <>
-            <div className="relative overflow-hidden rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F]">
-              <Flipped stagger flipId={isTransitioning ? `series-${series.id}` : 0}>
-                <img
-                  alt=""
-                  className="aspect-video w-full"
-                  loading={first ? 'eager' : 'lazy'}
-                  src={series.thumbnailUrl}
-                />
-              </Flipped>
-            </div>
-            <div className="p-[8px]">
-              <div className="line-clamp-2 text-[14px] font-bold text-[#ffffff]">{series.title}</div>
-            </div>
-          </>
-        );
-      }}
+      <div className="relative overflow-hidden rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F]">
+        <img alt="" className="aspect-video w-full" loading={first ? 'eager' : 'lazy'} src={series.thumbnailUrl} />
+      </div>
+      <div className="p-[8px]">
+        <div className="line-clamp-2 text-[14px] font-bold text-[#ffffff]">{series.title}</div>
+      </div>
     </NavLink>
   );
 };
