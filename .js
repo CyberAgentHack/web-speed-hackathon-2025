c7913fd7cfbc1b28b5844aa87c9bc73c432aac39
WatermelonBomb <coins07311102@gmail.com>
2025-03-22 23:09:54 +0900

    Update .js

--- .js
+++ .js
@@ -1,41 +1,41 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
-export default {
-  mode: 'production', 
-  entry: './src/index.tsx', 
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',  // キャッシュ対策として contenthash を付与
-    clean: true, // ビルド時に dist フォルダをクリーン
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx', '.mjs'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.[jt]sx?$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
+// export default {
+//   mode: 'production', 
+//   entry: './src/index.tsx', 
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: '[name].[contenthash].js',  // キャッシュ対策として contenthash を付与
+//     clean: true, // ビルド時に dist フォルダをクリーン
+//   },
+//   resolve: {
+//     extensions: ['.js', '.ts', '.tsx', '.mjs'],
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.[jt]sx?$/,
+//         use: 'babel-loader',
+//         exclude: /node_modules/,
+//       },
      
-    ],
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all', 
+//     ],
+//   },
+//   optimization: {
+//     splitChunks: {
+//       chunks: 'all', 
       
-    },
-    runtimeChunk: 'single', 
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-};
+//     },
+//     runtimeChunk: 'single', 
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: './public/index.html',
+//     }),
+//   ],
+// };
