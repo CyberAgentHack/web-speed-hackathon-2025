016d2d8f022fcc8d8cb72f1cdf2f5439f2fc1680
yoshioka <yosshioka0022@gmail.com>
2025-03-22 21:44:38 +0900

    RecommendedSectionを遅延読み込みするためにlazyとSuspenseを追加

--- workspaces/client/src/pages/home/components/HomePage.tsx
+++ workspaces/client/src/pages/home/components/HomePage.tsx
@@ -1,6 +1,13 @@
+import { lazy, Suspense } from 'react';
+
 import { createStore } from '@wsh-2025/client/src/app/createStore';
-import { RecommendedSection } from '@wsh-2025/client/src/features/recommended/components/RecommendedSection';
 import { useRecommended } from '@wsh-2025/client/src/features/recommended/hooks/useRecommended';
+
+const RecommendedSection = lazy(() =>
+  import('@wsh-2025/client/src/features/recommended/components/RecommendedSection').then((module) => ({
+    default: module.RecommendedSection,
+  })),
+);
 
 export const prefetch = async (store: ReturnType<typeof createStore>) => {
   const modules = await store
@@ -17,13 +24,15 @@ export const HomePage = () => {
       <title>Home - AremaTV</title>
 
       <div className="w-full py-[48px]">
-        {modules.map((module) => {
-          return (
-            <div key={module.id} className="mb-[24px] px-[24px]">
-              <RecommendedSection module={module} />
-            </div>
-          );
-        })}
+        <Suspense fallback={<div>Loading...</div>}>
+          {modules.map((module) => {
+            return (
+              <div key={module.id} className="mb-[24px] px-[24px]">
+                <RecommendedSection module={module} />
+              </div>
+            );
+          })}
+        </Suspense>
       </div>
     </>
   );
