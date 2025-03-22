63cc6865865f11a53786355f5922f2f36cb5fa6c
Edward Fox <edwardkenfox@gmail.com>
2025-03-22 14:02:36 +0900

    empty

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -73,16 +73,9 @@ export function registerSsr(app: FastifyInstance): void {
         <head>
           <meta charSet="UTF-8" />
           <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-          <script src="/public/main.js"></script>
         </head>
         <body></body>
       </html>
-      <script>
-        window.__staticRouterHydrationData = ${JSON.stringify({
-          actionData: context.actionData,
-          loaderData: context.loaderData,
-        })};
-      </script>
     `);
   });
 }
