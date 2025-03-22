a872d359e2169eba101aa11df3841cbd2bf18dd3
Asahi <82006976+toasahi@users.noreply.github.com>
2025-03-22 17:02:09 +0900

    不要な依存関係とポリフィルを削除し、SSRコードを整理

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -6,7 +6,6 @@ import webpack from 'webpack';
 
 /** @type {import('webpack').Configuration} */
 const config = {
-  devtool: 'inline-source-map',
   entry: './src/main.tsx',
   mode: 'production',
   module: {
@@ -24,10 +23,9 @@ const config = {
               [
                 '@babel/preset-env',
                 {
-                  corejs: '3.41',
-                  forceAllTransforms: true,
-                  targets: 'defaults',
-                  useBuiltIns: 'entry',
+                  targets: {
+                    chrome: '134',
+                  }
                 },
               ],
               ['@babel/preset-react', { runtime: 'automatic' }],
