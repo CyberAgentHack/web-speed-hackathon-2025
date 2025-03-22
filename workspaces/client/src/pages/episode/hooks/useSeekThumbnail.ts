6cdce0d4ce186d8f0bf9dc30291f85774dcdf7a8
ayumu-1212 <ayumuabe1434@gmail.com>
2025-03-22 22:00:14 +0900

    update: ffmpegをこれで動的importできるらしいよ

--- workspaces/client/src/pages/episode/hooks/useSeekThumbnail.ts
+++ workspaces/client/src/pages/episode/hooks/useSeekThumbnail.ts
@@ -1,4 +1,3 @@
-import { FFmpeg } from '@ffmpeg/ffmpeg';
 import { StandardSchemaV1 } from '@standard-schema/spec';
 import * as schema from '@wsh-2025/schema/src/api/schema';
 import { Parser } from 'm3u8-parser';
@@ -15,6 +14,9 @@ async function getSeekThumbnail({ episode }: Params) {
   parser.push(await fetch(playlistUrl).then((res) => res.text()));
   parser.end();
 
+  // FFmpeg の動的インポート
+  const { FFmpeg } = await import('@ffmpeg/ffmpeg');
+  
   // FFmpeg の初期化
   const ffmpeg = new FFmpeg();
   await ffmpeg.load({
