b31743ed69cbe666b0e1c509ac09b4043b8d898d
yamanoku <0910yama@gmail.com>
2025-03-22 18:26:41 +0900

    fluent iconをSVGに置き換える

--- workspaces/client/src/pages/program/components/PlayerController.tsx
+++ workspaces/client/src/pages/program/components/PlayerController.tsx
@@ -11,10 +11,9 @@ export const PlayerController = () => {
       <div className="absolute inset-x-0 bottom-0 px-[12px]">
         <div className="flex w-full flex-row items-center justify-between">
           <div className="flex flex-row items-center">
-            <span className="i-fluent:live-24-filled m-[14px] block size-[20px] shrink-0 grow-0 text-[#FFFFFF]" />
+            <svg className="m-[14px] block size-[20px] shrink-0 grow-0" width="24" height="24" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M6.343 4.938a1 1 0 0 1 0 1.415 8.003 8.003 0 0 0 0 11.317 1 1 0 1 1-1.415 1.414c-3.906-3.906-3.906-10.24 0-14.146a1 1 0 0 1 1.415 0Zm12.731 0c3.906 3.907 3.906 10.24 0 14.146a1 1 0 0 1-1.414-1.414 8.003 8.003 0 0 0 0-11.317 1 1 0 0 1 1.414-1.415ZM9.31 7.812a1 1 0 0 1 0 1.414 3.92 3.92 0 0 0 0 5.544 1 1 0 1 1-1.414 1.414 5.92 5.92 0 0 1 0-8.372 1 1 0 0 1 1.414 0Zm6.959 0a5.92 5.92 0 0 1 0 8.372 1 1 0 0 1-1.415-1.414 3.92 3.92 0 0 0 0-5.544 1 1 0 0 1 1.415-1.414Zm-4.187 2.77a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" fill="#FFFFFF"/></svg>
             <span className="ml-[4px] block shrink-0 grow-0 text-[12px] font-bold text-[#FFFFFF]">ライブ配信</span>
           </div>
-
           <div className="flex flex-row items-center">
             <Hoverable classNames={{ default: 'bg-transparent', hovered: 'bg-[#FFFFFF1F]' }}>
               <button
