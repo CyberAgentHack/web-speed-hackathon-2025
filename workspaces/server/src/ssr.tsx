a931dcb384b1ae924d1913a7113c1bcfa0288405
tosaken1116 <tosaken.dev@gmail.com>
2025-03-23 02:57:25 +0900

    Update SSR to inject rendered HTML into the root div and remove unused file path functions

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -1,4 +1,4 @@
-import { promises, readdirSync } from 'node:fs';
+import { promises } from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -12,18 +12,6 @@ import htmlescape from 'htmlescape';
 import { StrictMode } from 'react';
 import { renderToString } from 'react-dom/server';
 import { createStaticHandler, createStaticRouter, StaticRouterProvider } from 'react-router';
-
-function getFiles(parent: string): string[] {
-  const dirents = readdirSync(parent, { withFileTypes: true });
-  return dirents
-    .filter((dirent) => dirent.isFile() && !dirent.name.startsWith('.'))
-    .map((dirent) => path.join(parent, dirent.name));
-}
-
-function getFilePaths(relativePath: string, rootDir: string): string[] {
-  const files = getFiles(path.resolve(rootDir, relativePath));
-  return files.map((file) => path.join('/', path.relative(rootDir, file)));
-}
 
 export function registerSsr(app: FastifyInstance): void {
   app.register(fastifyStatic, {
@@ -41,17 +29,20 @@ export function registerSsr(app: FastifyInstance): void {
   app.get('/*', async (req, reply) => {
     // @ts-expect-error ................
     const request = createStandardRequest(req, reply);
-
+    console.time('SSR');
     const store = createStore({});
-    const handler = createStaticHandler(createRoutes(store));
+    const routes = createRoutes(store);
+    const handler = createStaticHandler(routes);
     const context = await handler.query(request);
+
+    console.timeEnd('SSR');
 
     if (context instanceof Response) {
       return reply.send(context);
     }
 
     const router = createStaticRouter(handler.dataRoutes, context);
-    renderToString(
+    const renderd = renderToString(
       <StrictMode>
         <StoreProvider createStore={() => store}>
           <StaticRouterProvider context={context} hydrate={false} router={router} />
@@ -59,22 +50,10 @@ export function registerSsr(app: FastifyInstance): void {
       </StrictMode>,
     );
 
-    const rootDir = path.resolve(__dirname, '../../../');
-    const imagePaths = [
-      getFilePaths('public/images', rootDir),
-      getFilePaths('public/animations', rootDir),
-      getFilePaths('public/logos', rootDir),
-    ].flat();
-
     const clientHTML = path.resolve(__dirname, '../../client/dist/index.html');
     const clientHTMLContent = await promises.readFile(clientHTML, 'utf-8');
-    const dataFetchHTML = `<script>
-        window.__staticRouterHydrationData = ${htmlescape({
-          actionData: context.actionData,
-          loaderData: context.loaderData,
-        })};
-      </script>`;
-    const modifiedHTMLContent = clientHTMLContent.replace('<!-- DATA_FETCH_HYDRATION -->', dataFetchHTML);
-    reply.type('text/html').send(modifiedHTMLContent);
+
+    const replaced = clientHTMLContent.replace('<!-- __REACT_APP_HTML__ -->', renderd);
+    reply.type('text/html').send(replaced);
   });
 }
