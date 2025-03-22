0e9ae40e28b9b0b2d2e8945f3755f1eb8325e4c5
WatermelonBomb <coins07311102@gmail.com>
2025-03-22 16:05:48 +0900

    Create users.ts

---
+++ workspaces/server/src/routes/users.ts
@@ -0,0 +1,22 @@
+import { FastifyInstance } from 'fastify';
+import Redis from 'ioredis';
+
+const redis = new Redis(); 
+
+export default async function userRoutes(app: FastifyInstance) {
+  app.get('/users/:id', async (request, reply) => {
+    const userId = request.params.id;
+    const cacheKey = `user-${userId}`;
+
+    let user = await redis.get(cacheKey);
+    if (user) {
+      return JSON.parse(user);
+    }
+
+    user = await db.query('SELECT * FROM users WHERE id = ?', [userId]);
+
+    redis.set(cacheKey, JSON.stringify(user), 'EX', 60 * 5);
+
+    return user;
+  });
+}
