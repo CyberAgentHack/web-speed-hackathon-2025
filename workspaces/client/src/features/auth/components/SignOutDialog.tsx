b8cede1df718d0576ba945b40c9e70ad549b905a
rtrtn <rtro1407@gmail.com>
2025-03-22 14:46:51 +0900

    fix: SignOutDialog

--- workspaces/client/src/features/auth/components/SignOutDialog.tsx
+++ workspaces/client/src/features/auth/components/SignOutDialog.tsx
@@ -1,5 +1,4 @@
-import { FORM_ERROR } from 'final-form';
-import { Form } from 'react-final-form';
+import { useState } from 'react';
 
 import { useAuthActions } from '@wsh-2025/client/src/features/auth/hooks/useAuthActions';
 import { Dialog } from '@wsh-2025/client/src/features/dialog/components/Dialog';
@@ -12,17 +11,7 @@ interface Props {
 export const SignOutDialog = ({ isOpen, onClose }: Props) => {
   const authActions = useAuthActions();
 
-  const onSubmit = async () => {
-    try {
-      await authActions.signOut();
-
-      alert('ログアウトしました');
-      onClose();
-      return;
-    } catch {
-      return { [FORM_ERROR]: '不明なエラーが発生しました' };
-    }
-  };
+  const [errorMessage, setErrorMessage] = useState('');
 
   return (
     <Dialog isOpen={isOpen} onClose={onClose}>
@@ -33,32 +22,41 @@ export const SignOutDialog = ({ isOpen, onClose }: Props) => {
 
         <h2 className="mb-[24px] text-center text-[24px] font-bold">ログアウト</h2>
 
-        <Form onSubmit={onSubmit}>
-          {({ handleSubmit, submitError }) => (
-            <form className="mb-[16px]" onSubmit={(ev) => void handleSubmit(ev)}>
-              <div className="mb-[24px] flex w-full flex-row items-center justify-start rounded-[4px] border-[2px] border-solid border-[#DDAA00] bg-[#fffcee] p-[8px] text-[14px] font-bold text-[#DDAA00]">
-                <div className="i-material-symbols:warning-outline-rounded m-[4px] size-[20px]" />
-                <span>プレミアムエピソードが視聴できなくなります。</span>
-              </div>
+        <form
+          action={async () => {
+            await authActions
+              .signOut()
+              .then(() => {
+                alert('ログアウトしました');
+                onClose();
+              })
+              .catch(() => {
+                setErrorMessage('不明なエラーが発生しました');
+              });
+          }}
+          className="mb-[16px]"
+        >
+          <div className="mb-[24px] flex w-full flex-row items-center justify-start rounded-[4px] border-[2px] border-solid border-[#DDAA00] bg-[#fffcee] p-[8px] text-[14px] font-bold text-[#DDAA00]">
+            <div className="i-material-symbols:warning-outline-rounded m-[4px] size-[20px]" />
+            <span>プレミアムエピソードが視聴できなくなります。</span>
+          </div>
 
-              {submitError ? (
-                <div className="mb-[8px] flex w-full flex-row items-center justify-start rounded-[4px] border-[2px] border-solid border-[#F0163A] bg-[#ffeeee] p-[8px] text-[14px] font-bold text-[#F0163A]">
-                  <div className="i-material-symbols:error-outline m-[4px] size-[20px]" />
-                  <span>{submitError}</span>
-                </div>
-              ) : null}
-
-              <div className="flex flex-row justify-center">
-                <button
-                  className="block flex w-[160px] flex-row items-center justify-center rounded-[4px] bg-[#1c43d1] p-[12px] text-[14px] font-bold text-[#ffffff] disabled:opacity-50"
-                  type="submit"
-                >
-                  ログアウト
-                </button>
-              </div>
-            </form>
+          {errorMessage && (
+            <div className="mb-[8px] flex w-full flex-row items-center justify-start rounded-[4px] border-[2px] border-solid border-[#F0163A] bg-[#ffeeee] p-[8px] text-[14px] font-bold text-[#F0163A]">
+              <div className="i-material-symbols:error-outline m-[4px] size-[20px]" />
+              <span>{errorMessage}</span>
+            </div>
           )}
-        </Form>
+
+          <div className="flex flex-row justify-center">
+            <button
+              className="block flex w-[160px] flex-row items-center justify-center rounded-[4px] bg-[#1c43d1] p-[12px] text-[14px] font-bold text-[#ffffff] disabled:opacity-50"
+              type="submit"
+            >
+              ログアウト
+            </button>
+          </div>
+        </form>
       </div>
     </Dialog>
   );
