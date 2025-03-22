c3b5d3236806b137a7b6dd51fb12d910ff780a43
newt <ysti2019.win2020.st@gmail.com>
2025-03-22 16:33:50 +0900

    再度@babel/preset-envを変更

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -20,14 +20,7 @@ const config = {
           options: {
             cacheDirectory: true,
             presets: [
-              [
-                '@babel/preset-env',
-                {
-                  corejs: '3.41',
-                  targets: 'defaults',
-                  useBuiltIns: 'entry',
-                },
-              ],
+              ['@babel/preset-env', { modules: false }],
               ['@babel/preset-react', { runtime: 'automatic' }],
               ['@babel/preset-typescript'],
             ],
