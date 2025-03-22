49081aed5c1c2c38df6308733744a50470085275
ayumu-1212 <ayumuabe1434@gmail.com>
2025-03-23 00:36:52 +0900

    update: remove workspace dependency for @wsh-2025/client

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -79,6 +79,7 @@ const config = {
     alias: {
       '@ffmpeg/core$': path.resolve(import.meta.dirname, 'node_modules', '@ffmpeg/core/dist/umd/ffmpeg-core.js'),
       '@ffmpeg/core/wasm$': path.resolve(import.meta.dirname, 'node_modules', '@ffmpeg/core/dist/umd/ffmpeg-core.wasm'),
+      '@wsh-2025/client': import.meta.dirname,
     },
     extensions: ['.js', '.cjs', '.mjs', '.ts', '.cts', '.mts', '.tsx', '.jsx'],
   },
