244fd5d4d2ef43d80a4ebcb8dcdb47266d6b6d2c
high-g <altema1217bbfumi@live.jp>
2025-03-22 16:52:21 +0900

    fix: 戻し

--- workspaces/server/src/ssr.tsx
+++ workspaces/server/src/ssr.tsx
@@ -1,4 +1,4 @@
-import { readdirSync } from 'node:fs';
+// import { readdirSync } from 'node:fs';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -12,17 +12,17 @@ import htmlescape from 'htmlescape';
 import { renderToString } from 'react-dom/server';
 import { createStaticHandler, createStaticRouter, StaticRouterProvider } from 'react-router';
 
-function getFiles(parent: string): string[] {
-  const dirents = readdirSync(parent, { withFileTypes: true });
-  return dirents
-    .filter((dirent) => dirent.isFile() && !dirent.name.startsWith('.'))
-    .map((dirent) => path.join(parent, dirent.name));
-}
+// function getFiles(parent: string): string[] {
+//   const dirents = readdirSync(parent, { withFileTypes: true });
+//   return dirents
+//     .filter((dirent) => dirent.isFile() && !dirent.name.startsWith('.'))
+//     .map((dirent) => path.join(parent, dirent.name));
+// }
 
-function getFilePaths(relativePath: string, rootDir: string): string[] {
-  const files = getFiles(path.resolve(rootDir, relativePath));
-  return files.map((file) => path.join('/', path.relative(rootDir, file)));
-}
+// function getFilePaths(relativePath: string, rootDir: string): string[] {
+//   const files = getFiles(path.resolve(rootDir, relativePath));
+//   return files.map((file) => path.join('/', path.relative(rootDir, file)));
+// }
 
 export function registerSsr(app: FastifyInstance): void {
   app.register(fastifyStatic, {
@@ -56,12 +56,14 @@ export function registerSsr(app: FastifyInstance): void {
       </StoreProvider>,
     );
 
-    const rootDir = path.resolve(__dirname, '../../../');
-    const imagePaths = [
-      getFilePaths('public/images', rootDir),
-      getFilePaths('public/animations', rootDir),
-      getFilePaths('public/logos', rootDir),
-    ].flat();
+    // const rootDir = path.resolve(__dirname, '../../../');
+    // const imagePaths = [
+    //   getFilePaths('public/images', rootDir),
+    //   getFilePaths('public/animations', rootDir),
+    //   getFilePaths('public/logos', rootDir),
+    // ].flat();
+
+    // ${imagePaths.map((imagePath) => `<link as="image" href="${imagePath}" rel="preload" />`).join('\n')}
 
     reply.type('text/html').send(/* html */ `
       <!DOCTYPE html>
@@ -70,7 +72,6 @@ export function registerSsr(app: FastifyInstance): void {
           <meta charSet="UTF-8" />
           <meta content="width=device-width, initial-scale=1.0" name="viewport" />
           <script src="/public/main.js" defer></script>
-          ${imagePaths.map((imagePath) => `<link as="image" href="${imagePath}" rel="preload" />`).join('\n')}
         </head>
         <body>
           <div id="root">${appHtml}</div>
