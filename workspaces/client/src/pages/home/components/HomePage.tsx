f5df47f5e84d2cde7b9eec6a81ae363bc7cd8791
Luke256 <yuta.k.200628@gmail.com>
2025-03-23 00:04:30 +0900

    fastify homepage

--- workspaces/client/src/pages/home/components/HomePage.tsx
+++ workspaces/client/src/pages/home/components/HomePage.tsx
@@ -1,6 +1,11 @@
+import { StandardSchemaV1 } from '@standard-schema/spec';
+import * as schema from '@wsh-2025/schema/src/api/schema';
+import invariant from 'tiny-invariant';
+import { ArrayValues } from 'type-fest';
+
 import { createStore } from '@wsh-2025/client/src/app/createStore';
 import { RecommendedSection } from '@wsh-2025/client/src/features/recommended/components/RecommendedSection';
-import { useRecommended } from '@wsh-2025/client/src/features/recommended/hooks/useRecommended';
+// import { useRecommended } from '@wsh-2025/client/src/features/recommended/hooks/useRecommended';
 
 export const prefetch = async (store: ReturnType<typeof createStore>) => {
   const modules = await store
@@ -10,7 +15,16 @@ export const prefetch = async (store: ReturnType<typeof createStore>) => {
 };
 
 export const HomePage = () => {
-  const modules = useRecommended({ referenceId: 'entrance' });
+  // const modules = useRecommended({ referenceId: 'entrance' });
+  const hydrationData = window.__staticRouterHydrationData as {
+    loaderData?: {
+      "0-0"?: {
+        modules?: ArrayValues<StandardSchemaV1.InferOutput<typeof schema.getRecommendedModulesResponse>>[];
+      };
+    };
+  };
+  const modules = hydrationData.loaderData?.["0-0"]?.modules;
+  invariant(modules, 'Expected hydration data to include modules');
 
   return (
     <>
