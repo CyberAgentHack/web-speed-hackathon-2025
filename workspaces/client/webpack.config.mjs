16d77768f6c24e22ffda2147c5439828a5fbb3a6
KaiOuchi <30932721+ouchikai@users.noreply.github.com>
2025-03-22 11:45:54 +0900

    [画像圧縮] jpegoptim --max=10 *.jpeg

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -1,7 +1,7 @@
 import path from 'node:path';
 
 import webpack from 'webpack';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+// import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 
 /** @type {import('webpack').Configuration} */
 const config = {
@@ -62,7 +62,7 @@ const config = {
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
     new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: '' }),
-    new BundleAnalyzerPlugin(),
+    // new BundleAnalyzerPlugin(),
   ],
   resolve: {
     alias: {
