61625d19fbd095df18f9df3f279dbb39ed0a7567
cryptobox <pray.happy76@gmail.com>
2025-03-22 15:09:04 +0900

    不要なクエリを削除

--- workspaces/client/src/features/recommended/components/EpisodeItem.tsx
+++ workspaces/client/src/features/recommended/components/EpisodeItem.tsx
@@ -8,9 +8,6 @@ interface Props {
   episode: {
     id: string;
     premium: boolean;
-    series: {
-      title: string;
-    };
     thumbnailUrl: string;
     title: string;
   };
@@ -39,7 +36,7 @@ export const EpisodeItem = ({ episode }: Props) => {
                   <Ellipsis ellipsis reflowOnResize maxLine={2} text={episode.title} visibleLine={2} />
                 </div>
                 <div className="text-[12px] text-[#999999]">
-                  <Ellipsis ellipsis reflowOnResize maxLine={2} text={episode.series.title} visibleLine={2} />
+                  <Ellipsis ellipsis reflowOnResize maxLine={2} text={episode.title} visibleLine={2} />
                 </div>
               </div>
             </>
