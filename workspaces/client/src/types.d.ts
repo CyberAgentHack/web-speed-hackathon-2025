87cf0a8dd4b1aa23f925aac1f30b64a60f2e28d8
mae616 <oya2.mi@hotmail.co.jp>
2025-03-23 03:27:02 +0900

    デプロイエラーの修正

--- workspaces/client/src/types.d.ts
+++ workspaces/client/src/types.d.ts
@@ -3,6 +3,16 @@ declare module '*.png' {
   export = value;
 }
 
+declare module '*.svg' {
+  const value: string;
+  export = value;
+}
+
+declare module '*.css' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
+
 declare module '*?raw' {
   const value: string;
   export = value;
