be7bcc54eb411ba82042224a96926c9c9b715c4e
shukufukuto@mac <shukufukuto@gmail.com>
2025-03-23 12:56:58 +0900

    Recommendedの最適化

--- workspaces/configs/eslint.config.mjs
+++ workspaces/configs/eslint.config.mjs
@@ -42,6 +42,11 @@ const configs = [
       ...eslintPluginReact.configs.recommended.rules,
       ...eslintPluginReact.configs['jsx-runtime'].rules,
       '@typescript-eslint/no-invalid-void-type': ['off'],
+      '@typescript-eslint/no-explicit-any': ['off'],
+      '@typescript-eslint/no-unsafe-member-access': ['off'],
+      '@typescript-eslint/no-unsafe-assignment': ['off'],
+      '@typescript-eslint/no-unsafe-call': ['off'],
+      '@typescript-eslint/no-unsafe-return': ['off'],
       '@typescript-eslint/no-unused-vars': [
         'error',
         {
