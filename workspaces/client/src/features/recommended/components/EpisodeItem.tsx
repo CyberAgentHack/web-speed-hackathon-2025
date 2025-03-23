075b23cec36cfd56c7d12ffbb2b6808e59bfed56
batora9 <sm.net_minecraft.hacker@outlook.jp>
2025-03-23 16:56:26 +0900

    feat: バカでかiconライブラリからsvg

--- workspaces/client/src/features/recommended/components/EpisodeItem.tsx
+++ workspaces/client/src/features/recommended/components/EpisodeItem.tsx
@@ -25,7 +25,14 @@ export const EpisodeItem = ({ episode }: Props) => {
               <Flipped stagger flipId={isTransitioning ? `episode-${episode.id}` : 0}>
                 <div className="relative overflow-hidden rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F] before:absolute before:inset-x-0 before:bottom-0 before:block before:h-[64px] before:bg-gradient-to-t before:from-[#212121] before:to-transparent before:content-['']">
                   <img alt="" className="h-auto w-full" loading="lazy" src={episode.thumbnailUrl} />
-                  <span className="i-material-symbols:play-arrow-rounded absolute bottom-[4px] left-[4px] m-[4px] block size-[20px] text-[#ffffff]" />
+                  <span className="absolute bottom-[4px] left-[4px] m-[4px] block size-[20px] text-[#ffffff]">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                      <path
+                        fill="currentColor"
+                        d="M8 17.175V6.825q0-.425.3-.713t.7-.287q.125 0 .263.037t.262.113l8.15 5.175q.225.15.338.375t.112.475t-.112.475t-.338.375l-8.15 5.175q-.125.075-.262.113T9 18.175q-.4 0-.7-.288t-.3-.712"
+                      />
+                    </svg>
+                  </span>
                   {episode.premium ? (
                     <span className="absolute bottom-[8px] right-[4px] inline-flex items-center justify-center rounded-[4px] bg-[#1c43d1] p-[4px] text-[10px] text-[#ffffff]">
                       プレミアム
@@ -34,12 +41,8 @@ export const EpisodeItem = ({ episode }: Props) => {
                 </div>
               </Flipped>
               <div className="p-[8px]">
-                <div className="mb-[4px] text-[14px] font-bold text-[#ffffff] line-clamp-2">
-                  {episode.title}
-                </div>
-                <div className="text-[12px] text-[#999999] line-clamp-2">
-                  {episode.series.title}
-                </div>
+                <div className="mb-[4px] line-clamp-2 text-[14px] font-bold text-[#ffffff]">{episode.title}</div>
+                <div className="line-clamp-2 text-[12px] text-[#999999]">{episode.series.title}</div>
               </div>
             </>
           );
