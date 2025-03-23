083c3bbfe4ccaf6c0c15fc190037855c6bee34ab
jdkfx <htazoe@jdkfx.com>
2025-03-23 17:27:51 +0900

    cache-control

--- workspaces/server/src/index.ts
+++ workspaces/server/src/index.ts
@@ -14,7 +14,7 @@ async function main() {
   const app = fastify();
 
   app.addHook('onSend', async (_req, reply) => {
-    reply.header('cache-control', 'no-store');
+    reply.header('cache-control', 'public, max-age=31536000');
   });
   app.register(cors, {
     origin: true,
