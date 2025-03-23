db19610dad78754a38879dcd9b718b6952d42383
H524 <watasekoni6@gmail.com>
2025-03-23 03:47:10 +0000

    パッケージ関連ファイル修正

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -6,7 +6,7 @@ import webpack from 'webpack';
 const config = {
   // devtool: 'inline-source-map',
   entry: './src/main.tsx',
-  mode: 'none',
+  // mode: 'development',
   module: {
     rules: [
       {
