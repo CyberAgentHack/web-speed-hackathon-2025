6cb1a9202d17faf20a25a0c6617a0dc813f06e50
cryptobox <pray.happy76@gmail.com>
2025-03-23 15:17:39 +0900

    entrance以外はmoduleのみでOK

--- workspaces/server/src/api.ts
+++ workspaces/server/src/api.ts
@@ -493,8 +493,7 @@ export async function registerApi(app: FastifyInstance): Promise<void> {
         },
       });
 
-      // referenceIdがerrorの場合は最初のモジュールのみを返す
-      const result = req.params.referenceId === 'error' ? modules.slice(0, 1) : modules;
+      const result = req.params.referenceId !== 'entrance' ? modules.slice(0, 1) : modules;
       return sendJson(reply, 200, result);
     },
   });
