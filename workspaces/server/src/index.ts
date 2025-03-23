7ef0bfcb365051d34d115e3bf35ed283a2f0fbe0
Unidentified_Sushi <72974574+Err0r-SOZGGway@users.noreply.github.com>
2025-03-23 12:45:17 +0900

    http2通信が出来るように変更

--- workspaces/server/src/index.ts
+++ workspaces/server/src/index.ts
@@ -2,6 +2,8 @@ import '@wsh-2025/server/src/setups/luxon';
 
 import cors from '@fastify/cors';
 import fastify from 'fastify';
+import { readFileSync } from 'node:fs';
+import { resolve } from 'node:path';
 
 import { registerApi } from '@wsh-2025/server/src/api';
 import { initializeDatabase } from '@wsh-2025/server/src/drizzle/database';
@@ -11,11 +13,18 @@ import { registerStreams } from '@wsh-2025/server/src/streams';
 async function main() {
   await initializeDatabase();
 
-  const app = fastify();
+  const app = fastify({
+    http2: true,
+    https: {
+      key: readFileSync(resolve(__dirname, '../ssl/key.pem')),
+      cert: readFileSync(resolve(__dirname, '../ssl/cert.pem'))
+    }
+  });
 
   app.addHook('onSend', async (_req, reply) => {
     reply.header('cache-control', 'no-store');
   });
+
   app.register(cors, {
     origin: true,
   });
@@ -24,7 +33,10 @@ async function main() {
   app.register(registerSsr);
 
   await app.ready();
-  const address = await app.listen({ host: '0.0.0.0', port: Number(process.env['PORT']) });
+  const address = await app.listen({
+    host: '0.0.0.0',
+    port: Number(process.env['PORT'])
+  });
   console.log(`Server listening at ${address}`);
 }
 
