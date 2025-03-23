b0b6d9b1fea40facc0f0244b3e568d64dfeb8db2
yhfs21 <yfuse04510@gmail.com>
2025-03-23 16:22:22 +0900

    Revert "lazy load"
    This reverts commit 11db2904a640a7f8147f3e8dd024aa4e03001de8.

--- workspaces/client/src/pages/series/components/SeriesPage.tsx
+++ workspaces/client/src/pages/series/components/SeriesPage.tsx
@@ -35,7 +35,6 @@ export const SeriesPage = () => {
         <header className="mb-[24px] flex w-full flex-row items-start justify-between gap-[24px]">
           <Flipped stagger flipId={`series-${series.id}`}>
             <img
-              loading="lazy"
               alt=""
               className="h-auto w-[400px] shrink-0 grow-0 rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F]"
               src={series.thumbnailUrl}
