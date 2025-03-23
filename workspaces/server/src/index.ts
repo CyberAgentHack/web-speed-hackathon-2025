5126de79088eb3bfc40ccaea4d156885836b94ad
nkn-ms <nkn-ms@hotmail.co.jp>
2025-03-23 17:10:40 +0900

    wip

--- workspaces/server/src/index.ts
+++ workspaces/server/src/index.ts
@@ -2,6 +2,7 @@ import '@wsh-2025/server/src/setups/luxon';
 
 import cors from '@fastify/cors';
 import fastify from 'fastify';
+import fs from 'fs'
 
 import { registerApi } from '@wsh-2025/server/src/api';
 import { initializeDatabase } from '@wsh-2025/server/src/drizzle/database';
@@ -12,9 +13,17 @@ async function main() {
   await initializeDatabase();
 
   const app = fastify();
+  // const app = fastify({
+  //   http2: true,
+  //   https: {
+  //     key: fs.readFileSync('./key.pem'), // SSL証明書のキー
+  //     cert: fs.readFileSync('./cert.pem'), // SSL証明書
+  //   },
+  // });
 
   app.addHook('onSend', async (_req, reply) => {
-    reply.header('cache-control', 'no-store');
+    // reply.header('cache-control', 'no-store');
+    reply.header('cache-control', 'public, max-age=3600');
   });
   app.register(cors, {
     origin: true,
