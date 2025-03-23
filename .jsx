d41b893b1e4f4e13862c557d1d66d246dbe55d96
WatermelonBomb <coins07311102@gmail.com>
2025-03-23 16:53:18 +0900

    Update .jsx

--- .jsx
+++ .jsx
@@ -1,25 +1,25 @@
 
-// import React, { Suspense, lazy } from 'react';
-// import { Routes, Route } from 'react-router-dom';
+import React, { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-// const VideoPlayerPage = lazy(() => import('./pages/VideoPlayerPage'));
+const VideoPlayerPage = lazy(() => import('./pages/VideoPlayerPage'));
 
 
-// function App() {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<HomePage />} />
-//       <Route
-//         path="/video"
-//         element={
-//           <Suspense fallback={<div>Loading...</div>}>
-//             <VideoPlayerPage />
-//           </Suspense>
-//         }
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route
+        path="/video"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <VideoPlayerPage />
+          </Suspense>
+        }
       
-//       {/* ...ほかのルート */}
-//     </Routes>
-//   );
-// }
+      {/* ...ほかのルート */}
+    </Routes>
+  );
+}
 
-// export default App;
+export default App;
