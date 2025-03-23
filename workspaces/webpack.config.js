bc278c438411e5c1fcf04af6c7646782698281bb
WatermelonBomb <coins07311102@gmail.com>
2025-03-23 16:53:07 +0900

    Update webpack.config.js

--- workspaces/webpack.config.js
+++ workspaces/webpack.config.js
@@ -1,28 +1,28 @@
-// module.exports = {
+module.exports = {
 
-//   module: {
-//     rules: [
-//       {
-//         test: /\.tsx?$/,
-//         exclude: /node_modules/,
-//         use: {
-//           loader: 'swc-loader',
-//           options: {
-//             jsc: {
-//               parser: {
-//                 syntax: 'typescript',
-//                 tsx: true,
-//                 decorators: true
-//               },
-//               transform: {
-//                 react: {
-//                   runtime: 'automatic'
-//                 }
-//               }
-//             }
-//           }
-//         }
-//       }
-//     ]
-//   }
-// };
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'swc-loader',
+          options: {
+            jsc: {
+              parser: {
+                syntax: 'typescript',
+                tsx: true,
+                decorators: true
+              },
+              transform: {
+                react: {
+                  runtime: 'automatic'
+                }
+              }
+            }
+          }
+        }
+      }
+    ]
+  }
+};
