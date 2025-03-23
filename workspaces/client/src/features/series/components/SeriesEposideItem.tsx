f727481841080377dc9c7f677cc4edf6f8c1a7d9
Toshiki Itai <toshiki.itai@linecorp.com>
2025-03-23 18:29:50 +0900

    fix

--- workspaces/client/src/features/series/components/SeriesEposideItem.tsx
+++ workspaces/client/src/features/series/components/SeriesEposideItem.tsx
@@ -28,7 +28,7 @@ export const SeriesEpisodeItem = ({ episode, selected }: Props) => {
             <>
               <Flipped stagger flipId={!selected && isTransitioning ? `episode-${episode.id}` : 0}>
                 <div className="relative shrink-0 grow-0 overflow-hidden rounded-[8px] border-[2px] border-solid border-[#FFFFFF1F] before:absolute before:inset-x-0 before:bottom-0 before:block before:h-[64px] before:bg-gradient-to-t before:from-[#212121] before:to-transparent before:content-['']">
-                  <img alt="" className="h-[64px] w-[192px]" src={episode.thumbnailUrl} loading="lazy" />
+                  <img alt="" className="h-[108px] w-[192px]" src={episode.thumbnailUrl} loading="lazy" />
                   <Icon
                     icon="material-symbols:play-arrow-rounded"
                     className="absolute bottom-[4px] left-[4px] m-[4px] block size-[20px] text-[#ffffff]"
