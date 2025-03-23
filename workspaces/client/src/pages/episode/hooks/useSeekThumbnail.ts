f6e5bf2a606da22111a7422e966f1dc110e8b203
cryptobox <pray.happy76@gmail.com>
2025-03-23 13:20:36 +0900

    サムネイルがちらつくバグ修正

--- workspaces/client/src/pages/episode/hooks/useSeekThumbnail.ts
+++ workspaces/client/src/pages/episode/hooks/useSeekThumbnail.ts
@@ -1,16 +1,10 @@
 import { StandardSchemaV1 } from '@standard-schema/spec';
 import * as schema from '@wsh-2025/schema/src/api/schema';
-import { use } from 'react';
 
 interface Params {
   episode: StandardSchemaV1.InferOutput<typeof schema.getEpisodeByIdResponse>;
 }
 
-function getSeekThumbnail(_params: Params): Promise<string> {
-  // 一時的に静的な画像を返す
-  return Promise.resolve('/public/images/001.webp');
-}
-
 export function useSeekThumbnail(params: Params): string {
-  return use(getSeekThumbnail(params));
+  return params.episode.thumbnailUrl;
 }
