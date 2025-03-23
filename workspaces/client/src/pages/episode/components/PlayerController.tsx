cde19dca020a8cc7f0d6a58ef4f1a990afb57713
cp-20 <n.naoki.1587@gmail.com>
2025-03-23 14:32:34 +0900

    fix

--- workspaces/client/src/pages/episode/components/PlayerController.tsx
+++ workspaces/client/src/pages/episode/components/PlayerController.tsx
@@ -11,7 +11,7 @@ import { usePlaying } from '@wsh-2025/client/src/pages/episode/hooks/usePlaying'
 
 const formatSeconds = (seconds: number) => {
   const minute = Math.floor(seconds / 60);
-  const second = seconds % 60;
+  const second = Math.floor(seconds % 60);
   return `${String(minute).padStart(2, '0')}:${String(second).padStart(2, '0')}`;
 };
 
